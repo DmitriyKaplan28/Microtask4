e64c326fc8670245d13ec1adc4e6d6845c50039d
DmitriyKaplan28 <dsk007@mail.ru>
2022-03-31 19:34:38 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {MouseEvent, useState} from 'react';
 import './App.css';
+import {Button} from "./components/Button";
 
 function App() {
+
+ // let a = 1
+    let [a, setA] = useState(1)
+
+    const onClickHandler=() =>{
+        setA(++a);
+      console.log(a)
+    }
+    const onClickHandler2=() =>{
+        setA(a=0);
+      console.log(a)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>{a}</h1>
+          <button onClick={onClickHandler}>number</button>
+          <button onClick={onClickHandler2}>0</button>
+      </div>
   );
 }
 
